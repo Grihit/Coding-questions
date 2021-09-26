5a021395b6f5b7bac9aa77ba1ad5e5785ca8d044
Grihit <grihitbudhiraja12@gmail.com>
2021-09-26 19:40:01 +0530

    CanSum fix

--- CanSum.cpp
+++ CanSum.cpp
@@ -1,21 +1,25 @@
+//Can the target sum be created by addition of a set of numbers or not.
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 
 using namespace std;
 
-bool calculate(int target,int num[],unordered_map<int,bool>&memo){
-    if(memo.find(target) != memo.end())
+bool calculate(int target,int num[],unordered_map<int,bool>&memo,int n){
+    if(memo.find(target) != memo.end()){
         return memo[target];
-    if(target == 0)
+    }
+    if(target == 0){
         return true;
-    if(target<0)
+    }
+    if(target<0){
         return false;
-    for(int i=0;i<sizeof(num)/sizeof(num[0]);++i)
+    }
+    for(int i=0;i<n;i++)
     {
         //cout<<num[i]<<" "<<target<<endl;
         int diff = target - num[i];
-        if(calculate(diff,num,memo))
+        if(calculate(diff,num,memo,n))
         {
             memo[target] = true;
             return true;
@@ -27,10 +31,12 @@ bool calculate(int target,int num[],unordered_map<int,bool>&memo){
 
 int main()
 {
-    int target;
+    int target,c=0;
     unordered_map<int,bool> memo;
     int num[] = {7,14};
-    cout<<calculate(300,num,memo);
+    int n = sizeof(num)/sizeof(num[0]);
+    cout<<calculate(300,num,memo,n)<<endl;
+    //cout<<endl<<c;
     //for (auto x : memo)
       //cout << x.first << " " << x.second << endl;
 }
