7d1994bf87c5d218f38c9cda085cd645fce72326
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2021-11-05 16:35:51 +0530

    Create Arranging coins.cpp

---
+++ Arranging coins.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int count=0;
+        for(int i=1;;++i)
+        {
+            n = n-i;
+            if(n>=0)
+                count++;
+            else
+                break;
+        }
+        return count;
+    }
+};
