bf6aa507a2b97110af771d26c482a12ec1499c5a
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2021-11-26 16:26:39 +0530

    Create Search Insert.cpp

---
+++ Search Insert.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size();
+        int mid;
+        while(low<high)
+        {
+            mid = (low + high)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+            {
+                low = mid + 1;
+                mid = low;
+            }
+            else if(nums[mid]>target)
+            {
+                high = mid - 1;
+                mid = high;
+            }
+        }
+        if(mid == -1)
+            mid = 0;
+        if(mid == nums.size())
+            mid = nums.size()-1;
+        if(nums[mid]>=target)
+            return mid;
+        return mid+1;
+    }
+};
