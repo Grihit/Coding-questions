68f87bbd16cf7fb3ab5331942aafd330b0e12b18
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2022-02-07 20:41:31 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size()-1;
-        int mid;
+        int low = 0, high = nums.size()-1;
         while(high>=low)
         {
-            mid = low + (high - low)/2;
+            int mid = low + (high - low)/2;
             if(nums[mid] == target)
                 return mid;
             if(nums[mid]>target)
