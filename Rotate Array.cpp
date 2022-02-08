5b9676a4ab1ccda141ec3b1a60d754d73f10044b
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2022-02-08 19:16:18 +0530

    Create Rotate Array.cpp

---
+++ Rotate Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void reverse(vector<int>& nums,int i, int j){
+        int tmp = 0;       
+        while(i < j){
+            tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+    
+    void rotate(vector<int>& nums, int k) {
+        k %=nums.size();
+        reverse(nums, 0, nums.size() - k - 1);
+        reverse(nums, nums.size() - k, nums.size() - 1);
+        reverse(nums, 0, nums.size() - 1);
+    }
+};
