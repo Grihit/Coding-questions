25a81d660cd03f22550dee7b853a6236cd8997a8
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2022-03-23 15:30:10 +0530

    Create Broken Calculator.cpp

---
+++ Miscellaneous/Broken Calculator.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int ans = 0;
+        while(target > startValue)
+        {
+            if(target % 2)
+                ++target;
+            else
+                target /= 2;
+            
+            ++ans;
+        }
+        return ans  + startValue - target;
+    }
+};
