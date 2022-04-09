141ba45fff50ed53d27817f529354d893ab65c6b
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2022-04-09 18:36:13 +0530

    Update Top K Frequent Elements.cpp

--- Miscellaneous/Top K Frequent Elements.cpp
+++ Miscellaneous/Top K Frequent Elements.cpp
@@ -24,3 +24,25 @@ public:
         return ans;
     }
 };
+
+//Using Priority Queue
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         vector<int> res; 
+//         unordered_map<int, int> m; 
+//         priority_queue<pair<int, int>> pq;  
+        
+//         for (auto a : nums) m[a]++; 
+//         for (auto a : m) {
+//             pq.push({a.second, a.first}); 
+//         }
+        
+//         for (int i = 0; i < k; ++i) {
+//             res.push_back(pq.top().second);
+//             pq.pop(); 
+//         }
+        
+//         return res; 
+//     }
+// };
