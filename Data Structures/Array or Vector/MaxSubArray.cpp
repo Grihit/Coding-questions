1f66824aa97975a5b129cfe8af26d4ed3fd460b7
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2022-06-15 16:22:31 +0530

    Update MaxSubArray.cpp

--- Data Structures/Array or Vector/MaxSubArray.cpp
+++ Data Structures/Array or Vector/MaxSubArray.cpp
@@ -1,24 +1,39 @@
+// class Solution {
+// public:
+//     int Max(int a,int b)
+//     {
+//         if(a>b)
+//             return a;
+//         else
+//             return b;
+//     }
+//     int maxSubArray(vector<int>& nums) {
+//         int max=nums[0];
+//         int sum=nums[0];
+//         for(int i=1;i<nums.size();++i)
+//         {
+//             sum=sum+nums[i];
+//             max=Max(max,sum);
+//             if(nums[i]>max)
+//                 max=nums[i];
+//             if(nums[i]>sum)
+//                 sum=nums[i];
+//         }
+//         return max;
+//     }
+// };
+
+
 class Solution {
 public:
-    int Max(int a,int b)
-    {
-        if(a>b)
-            return a;
-        else
-            return b;
-    }
     int maxSubArray(vector<int>& nums) {
-        int max=nums[0];
-        int sum=nums[0];
-        for(int i=1;i<nums.size();++i)
-        {
-            sum=sum+nums[i];
-            max=Max(max,sum);
-            if(nums[i]>max)
-                max=nums[i];
-            if(nums[i]>sum)
-                sum=nums[i];
+        int maxSum = INT_MIN;
+        int sum = 0;
+        for(int i=0;i<nums.size();++i){
+            sum = sum+nums[i];
+            maxSum = max(maxSum, sum);
+            sum = max(sum, 0);
         }
-        return max;
+        return maxSum;
     }
 };
