afff804e3d0f51d549f52091476bebde8539092c
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2022-06-23 16:57:13 +0530

    Update Linked List Cycle.cpp

--- Data Structures/Linked List/Linked List Cycle.cpp
+++ Data Structures/Linked List/Linked List Cycle.cpp
@@ -6,27 +6,51 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         if(head == NULL)
+//             return false;
+//         ListNode *temp = head;
+//         ListNode *temp2 = head;
+//         while(true)
+//         {
+//             temp = temp -> next;
+//             if(temp == NULL)
+//                 return false;
+//             if(temp2 -> next == NULL)
+//                 return false;
+//             temp2 = temp2 -> next -> next;
+//             if(temp2 == NULL)
+//                 return false;
+//             if(temp == temp2)
+//                 return true;
+//         }
+//         return false;
+        
+//     }
+// };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL)
-            return false;
-        ListNode *temp = head;
-        ListNode *temp2 = head;
-        while(true)
-        {
-            temp = temp -> next;
-            if(temp == NULL)
-                return false;
-            if(temp2 -> next == NULL)
-                return false;
-            temp2 = temp2 -> next -> next;
-            if(temp2 == NULL)
-                return false;
-            if(temp == temp2)
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast && fast->next && fast->next->next){
+            fast = fast -> next -> next;
+            slow = slow -> next;
+            if(fast == slow)
                 return true;
         }
         return false;
-        
     }
 };
+
