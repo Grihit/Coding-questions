dd467871bf575b8c947e9e70ff70fac95e62cbe5
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2022-06-24 19:56:01 +0530

    Update Linked List palindrome.cpp

--- Data Structures/Linked List/Linked List palindrome.cpp
+++ Data Structures/Linked List/Linked List palindrome.cpp
@@ -45,26 +45,40 @@ public:
 //Reverse Linked List Solution
 
 //Using O(1) space and O(n) time
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(!head || !head->next)
-            return true;
-        ListNode* rev = new ListNode(head->val);
-        ListNode* temp = head->next, *check = head;
-        while(temp)
-        {
-            ListNode* t = new ListNode(temp->val);
-            t->next = rev;
-            rev = t;
-            temp = temp->next;
+        ListNode *fast, *slow;
+        slow = fast = head;
+        while(fast && fast->next && fast->next->next){
+            fast = fast -> next -> next;
+            slow = slow -> next;
         }
-        while(check)
-        {
-            if(check->val != rev->val)
+        ListNode *rev = NULL;
+        ListNode *revStart = slow -> next;
+        while(revStart){
+            ListNode *temp = revStart;
+            revStart = revStart -> next;
+            temp -> next = rev;
+            rev = temp;
+        }
+        slow -> next = rev;
+        slow = slow -> next;
+        while(slow){
+            if(head -> val != slow -> val)
                 return false;
-            rev = rev->next;
-            check= check->next;
+            head = head -> next;
+            slow = slow -> next;
         }
         return true;
     }
