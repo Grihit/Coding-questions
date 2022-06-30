2888931e7e73959c30fe7c858ccf54e39d7d0c01
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2022-07-01 02:09:11 +0530

    Update Subset Sum.cpp

--- Algorithms/Recursion/Subset Sum.cpp
+++ Algorithms/Recursion/Subset Sum.cpp
@@ -1,17 +1,17 @@
-void cal(vector<int> &num, vector<int> &res, int sum, int i, int flag){
-    if(flag == 0)
+void cal(vector<int> &num, vector<int> &res, int sum, int i){       
+    if(i == num.size()){
         res.push_back(sum);
-    if(i == num.size())
         return;
+    }
     int newSum = sum + num[i];
-    cal(num,res,sum,i+1,1);
-    cal(num,res,newSum,i+1,0);
+    cal(num,res,sum,i+1);
+    cal(num,res,newSum,i+1);
 }
 
 vector<int> subsetSum(vector<int> &num)
 {
     vector<int> res;
-    cal(num, res, 0, 0, 0);
+    cal(num, res, 0, 0);
     sort(res.begin(),res.end());
     return res;
 }
