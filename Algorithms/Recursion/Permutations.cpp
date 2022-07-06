41437bb2a51c6b0a6532784551c488e91ea5c35e
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2022-07-06 21:02:10 +0530

    Update Permutations.cpp

--- Algorithms/Recursion/Permutations.cpp
+++ Algorithms/Recursion/Permutations.cpp
@@ -29,3 +29,31 @@ public:
         return res;
     }
 };
+
+
+//Takes less time
+// class Solution {
+// public:
+//     void f(vector<int>& nums,vector<vector<int>>&ans,vector<int>&v,vector<int>&vis){
+//         if(v.size()==nums.size()){
+//             ans.push_back(v);
+//         }
+//         for(int i=0;i<nums.size();i++){
+//             if(!vis[i]){
+//                 vis[i]=1;
+//                 v.push_back(nums[i]);
+//                 f(nums,ans,v,vis);
+//                 v.pop_back();
+//                 vis[i]=0;
+//             }
+//         }
+        
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>>ans;
+//         vector<int>v;
+//         vector<int>vis(nums.size(),0);
+//         f(nums,ans,v,vis);
+//         return ans;
+//     }
+// };
