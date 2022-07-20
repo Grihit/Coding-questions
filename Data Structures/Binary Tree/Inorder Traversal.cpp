31d1aceff59d0021108c5bc506880fbfe1e37a08
Grihit Budhiraja <59744486+Grihit@users.noreply.github.com>
2022-07-20 15:21:12 +0530

    Update Inorder Traversal.cpp

--- Data Structures/Binary Tree/Inorder Traversal.cpp
+++ Data Structures/Binary Tree/Inorder Traversal.cpp
@@ -9,38 +9,54 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         stack<TreeNode*> s;
+//         map<TreeNode*, bool> visited;
+//         vector<int> ans;
+//         if(root == NULL)
+//             return ans;
+//         s.push(root);
+//         while(!s.empty())
+//         {
+//             TreeNode* check = s.top();
+//             if(check -> left != NULL && !visited[check])
+//             {
+//                 s.push(check -> left);
+//                 visited[check] = true;
+//             }
+//             else
+//             {
+//                 ans.push_back(check -> val);
+//                 //cout<<check -> val;
+//                 TreeNode* temp = NULL;
+//                 if(check -> right != NULL)
+//                     temp = check -> right;
+//                 s.pop();
+//                 if(temp)
+//                 {
+//                     s.push(temp);
+//                 }
+//             }
+//         }
+//         return ans;
+        
+//     }
+// };
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        stack<TreeNode*> s;
-        map<TreeNode*, bool> visited;
-        vector<int> ans;
+    void inorder(TreeNode* root, vector<int> &res){
         if(root == NULL)
-            return ans;
-        s.push(root);
-        while(!s.empty())
-        {
-            TreeNode* check = s.top();
-            if(check -> left != NULL && !visited[check])
-            {
-                s.push(check -> left);
-                visited[check] = true;
-            }
-            else
-            {
-                ans.push_back(check -> val);
-                //cout<<check -> val;
-                TreeNode* temp = NULL;
-                if(check -> right != NULL)
-                    temp = check -> right;
-                s.pop();
-                if(temp)
-                {
-                    s.push(temp);
-                }
-            }
-        }
-        return ans;
-        
+            return;
+        inorder(root->left, res);
+        res.push_back(root -> val);
+        inorder(root->right, res);
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        inorder(root, res);
+        return res;
     }
 };
